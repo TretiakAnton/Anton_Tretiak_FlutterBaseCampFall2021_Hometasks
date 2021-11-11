ccaa838f9ee7524ac350aa90a74d065b64c21c0e
TretiakAnton <anton.v.tretyak@gmail.com>
2021-11-11 20:02:09 +0200

    bloc v.1

--- lib/hash_calculator/main_isolate_task_performer.dart
+++ lib/hash_calculator/main_isolate_task_performer.dart
@@ -1,11 +1,16 @@
-import 'package:lecture_2_hometask_starter/constants.dart';
 import 'package:lecture_2_hometask_starter/hash_calculator/heavy_task_performer.dart';
 import 'package:lecture_2_hometask_starter/helpers/random_number_hash_calculator.dart';
 
 class MainIsolateTaskPerformer implements HeavyTaskPerformer {
   @override
-  Future<String> doSomeHeavyWork() async {
+  Future<String> doSomeHeavyWork(int IterationsCount) async {
     final hashCalculator = RandomNumberHashCalculator();
-    return hashCalculator.calculateRandomNumberHash(iterationsCount: DefaultIterationsCount);
+    return hashCalculator.calculateRandomNumberHash(
+        iterationsCount: IterationsCount);
+  }
+
+  @override
+  void stopDoSomeHeavyWork() {
+    return;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,65 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:lecture_2_hometask_starter/hash_calculator/heavy_task_performer.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lecture_2_hometask_starter/hash_calculator/main_isolate_task_performer.dart';
 
+int _iterationCount = 0;
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void run(int iterationCount) {
+    final task = MainIsolateTaskPerformer();
+    String result;
+    result = task.doSomeHeavyWork(iterationCount) as String;
+    //emit(state += result!);
+  }
+
+  void stop() {
+    final task = MainIsolateTaskPerformer();
+    String result = 'Task stopped';
+    task.stopDoSomeHeavyWork();
+    emit(state); //how to give result?
+  }
+}
+
 void main() {
-  runApp(
-    MyApp(
-      taskPerformer: MainIsolateTaskPerformer(),
-    ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({
-    Key? key,
-    required this.taskPerformer,
-  }) : super(key: key);
-
-  final HeavyTaskPerformer taskPerformer;
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo default',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(
-        title: 'Flutter Demo Home Page',
-        taskPerformer: taskPerformer,
+      home: BlocProvider(
+        create: (_) => CounterCubit(),
+        child: MyHomePage(title: 'Flutter Demo Bloc'),
       ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    Key? key,
-    required this.title,
-    required this.taskPerformer,
-  }) : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
-  final HeavyTaskPerformer taskPerformer;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String heavyTaskResult = '';
-  bool isPerformingTask = false;
+  TextEditingController _iterationsController = TextEditingController();
+
+  _getData(String data) {
+    _iterationCount = int.parse(data);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,38 +68,66 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Heavy task result is equal to: $heavyTaskResult',
-                textAlign: TextAlign.center,
-              ),
-              isPerformingTask
-                  ? const CircularProgressIndicator()
-                  : ElevatedButton(
-                      onPressed: () async {
-                        setState(() {
-                          isPerformingTask = true;
-                          heavyTaskResult = '';
-                        });
-
-                        final taskResult =
-                            await widget.taskPerformer.doSomeHeavyWork();
-
-                        setState(() {
-                          isPerformingTask = false;
-                          heavyTaskResult = taskResult;
-                        });
-                      },
-                      child: const Text('Perform Heavy Task'),
-                    ),
-            ],
-          ),
-        ),
+        child: CounterTextContainerWidget(),
       ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          TextField(
+              keyboardType: TextInputType.number,
+              controller: _iterationsController,
+              decoration: InputDecoration(
+                  hintText: 'iterations count', border: OutlineInputBorder()),
+              onSubmitted: _getData),
+          FloatingActionButton(
+              onPressed: () =>
+                  context.read<CounterCubit>().run(_iterationCount),
+              tooltip: 'Run',
+              child: Icon(Icons.add)),
+          FloatingActionButton(
+            onPressed: () => context.read<CounterCubit>().stop(),
+            tooltip: 'Stop',
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CounterTextContainerWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'You have pushed the button this many times:',
+          style: TextStyle(color: Colors.red, fontSize: 20),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        CounterTextWidget(),
+      ],
+    );
+  }
+}
+
+class CounterTextWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.qr_code_rounded),
+        SizedBox(
+          width: 30,
+        ),
+        BlocBuilder<CounterCubit, int>(
+          builder: (context, result) => Center(child: Text('$result')),
+        ),
+      ],
     );
   }
 }
