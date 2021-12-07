45a37a2b2d6521483b6ebf9deeecf4f97a0efa5f
TretiakAnton <anton.v.tretyak@gmail.com>
2021-12-07 17:15:36 +0200

    name of test fixed

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,10 +1,6 @@
-import 'package:campnotes/repositories/user_repository.dart';
-import 'package:campnotes/user_dao.dart';
 import 'package:campnotes/widgets/auth_firebase.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/src/provider.dart';
-
-import '../main.dart';
 
 class Registration extends StatefulWidget {
   const Registration({Key key}) : super(key: key);
@@ -15,8 +11,6 @@ class Registration extends StatefulWidget {
 }
 
 class _RegistrationState extends State<Registration> {
-  String _email;
-  String _password;
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
 
--- lib/tests/integration_test.dart
+++ lib/tests/integration_test.dart
@@ -7,7 +7,7 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
-    testWidgets('tap on the floating action button, verify counter',
+    testWidgets('tap on the floating action button, go to the new screen',
         (WidgetTester tester) async {
       app.main();
       await tester.pumpAndSettle();
